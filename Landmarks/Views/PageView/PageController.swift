3b880968793207441b543f812dd753000aee84f0
JDTOmoi <jasondavetahir@gmail.com>
2024-04-19 10:41:33 +0700

    UIKit implemented

---
+++ Landmarks/Views/PageView/PageController.swift
@@ -0,0 +1,18 @@
+//
+//  PageController.swift
+//  0706012210036-Dave-AFL3
+//
+//  Created by student on 19/04/24.
+//
+
+import SwiftUI
+
+struct PageController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageController()
+}
